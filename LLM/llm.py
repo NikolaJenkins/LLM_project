e055becccc53c7f35af7eb84feb0f7444beecf22
naojoy <njoy3663@student.pps.net>
2025-02-09 17:07:38 -0800

    created model

--- LLM/llm.py
+++ LLM/llm.py
@@ -11,3 +11,24 @@ messages = [
 
 response = model.invoke(messages)
 print(response)
+
+# import vertexai
+# from vertexai.generative_models import GenerativeModel
+#
+# # TODO(developer): Update and un-comment below line
+# PROJECT_ID = "inductive-arena-449800-u8"
+# vertexai.init(project=PROJECT_ID, location="us-west1")
+#
+# model = GenerativeModel("gemini-1.5-flash-002")
+#
+# response = model.generate_content(
+#     "What's a good name for a flower shop that specializes in selling bouquets of dried flowers?"
+# )
+#
+# print(response.text)
+# # Example response:
+# # **Emphasizing the Dried Aspect:**
+# # * Everlasting Blooms
+# # * Dried & Delightful
+# # * The Petal Preserve
+# # ...
