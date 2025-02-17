42cc46675b1cfaaccbe34f7b1eac64f8c2c44a5f
naojoy <njoy3663@student.pps.net>
2025-02-16 17:04:32 -0800

    created model

--- LLM/llm.py
+++ LLM/llm.py
@@ -2,19 +2,36 @@
 
 from langchain_google_vertexai import ChatVertexAI
 from langchain_core.messages import HumanMessage, SystemMessage
+from langchain_core.tools import Tool
+from langchain_google_community import GoogleSearchAPIWrapper
+
+question = "What is the Biot-Savart Law?"
+
+search = GoogleSearchAPIWrapper()
+
+tool = Tool(
+    name="google_search",
+    description="Search Google for recent results.",
+    func=search.run,
+)
+
+output = tool.run(question)
+print(output)
 
 model = ChatVertexAI(model="gemini-1.5-flash")
 messages = [
-    SystemMessage("Translate the following from English into Italian"),
-    HumanMessage("hi!"),
+    SystemMessage("Answer the following question. Additional information is provided below."),
+    HumanMessage("What is the Biot-Savart Law?"),
+    SystemMessage("Additional information follows: "),
+    SystemMessage(output),
 ]
 
 response = model.invoke(messages)
 print(response)
 
-# import vertexai
-# from vertexai.generative_models import GenerativeModel
-#
+import vertexai
+from vertexai.generative_models import GenerativeModel
+
 # # TODO(developer): Update and un-comment below line
 # PROJECT_ID = "inductive-arena-449800-u8"
 # vertexai.init(project=PROJECT_ID, location="us-west1")
